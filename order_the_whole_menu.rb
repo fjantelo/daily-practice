539ab9cab3553bc475e38e3bdff60366c05b7a6d
Fernando Antelo <fjantelo@gmail.com>
2022-10-04 17:14:42 -0400

    Adds order the whole menu problem

---
+++ order_the_whole_menu.rb
@@ -0,0 +1,15 @@
+# Given a hash, where the keys are strings representing food items, and the values are numbers representing the price of each food, return the amount someone would pay if they'd order one of each food from the entire menu.
+
+# Input: {"hot dog" => 2, "hamburger" => 3, "steak sandwich" => 5, "fries" => 1, "cole slaw" => 1, "soda" => 2}
+
+# Output: 14
+
+# Explanation: If someone would order one of everything on the menu, they'd pay a total of 14 (the sum of all the hash's values).
+
+def whole_menu(input)
+  output = 0
+  input.each { |key, value| output = output + value }
+  return output
+end
+
+p whole_menu({ "hot dog" => 2, "hamburger" => 3, "steak sandwich" => 5, "fries" => 1, "cole slaw" => 1, "soda" => 2 })
