1f056a76999214d97deb784900f818b7304df0da
Fernando Antelo <fjantelo@gmail.com>
2022-12-12 13:29:34 -0500

    Adds logic for additional test cases to merge sorted arrays

--- merge_sorted_arrays.rb
+++ merge_sorted_arrays.rb
@@ -10,21 +10,30 @@ def merge_sorted_arrays(array1, array2)
   index1 = 0
   index2 = 0
   output = []
-  while index1 < array1.length && index2 < array2.length
-    if array1[index1] < array2[index2]
+  while index1 < array1.length || index2 < array2.length
+    if index2 == array2.length
+      output << array1[index1]
+      index1 += 1
+    elsif index1 == array1.length
+      output << array2[index2]
+      index2 += 1
+    elsif array2[index2] > array1[index1]
       output << array1[index1]
       index1 += 1
     elsif array1[index1] > array2[index2]
       output << array2[index2]
       index2 += 1
+    elsif array1[index1] == array2[index2]
+      output << array1[index1]
+      output << array2[index2]
+      index1 += 1
+      index2 += 1
     end
-  end
-  if array1[array1.length - 1] < array2[array2.length - 1]
-    output << array2[array2.length - 1]
-  elsif array1[array1.length - 1] > array2[array2.length - 1]
-    output << array1[array1.length - 1]
   end
   return output
 end
 
 p merge_sorted_arrays([1, 5, 8], [6, 9])
+p merge_sorted_arrays([1, 5, 8], [10, 12])
+p merge_sorted_arrays([1], [1])
+p merge_sorted_arrays([1, 5, 8], [6, 7])
